f19e9f1f4a0b9a3645d37f548350fde4e625031c
enaul <enaulad511@gmail.com>
2025-02-16 15:21:01 -0600

    creation 2
    printer_first_stage
    
    By Edisson Naula

--- files/constants.py
+++ files/constants.py
@@ -6,6 +6,6 @@ settings_path = "files/settings.json"
 flags_path = "files/flags.json"
 image_path_projector = "files/img/temp.png"
 server_domain = "http://raspberrypi.local/"
-server_domain = "http://localhost:5000/"
+server_domain1 = "http://localhost:5000/"
 base_url = "api/v1/printer"
 headers = {"Content-Type": "application/json"}
