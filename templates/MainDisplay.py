9f1cbf31da62da4fec6777d156d6c6c18f45feae
Weaver4 <edisson.naulad@gmail.com>
2025-05-27 11:07:10 -0600

    update fix start files missing

--- templates/MainDisplay.py
+++ templates/MainDisplay.py
@@ -270,8 +270,8 @@ class MainGUIDisplay(ttk.Window):
             self.state("zoomed")
         except Exception  as e:
             print(e)
-            # self.attributes("-zoomed", True)
-            self.attributes("-fullscreen", True)
+            self.attributes("-zoomed", True)
+            # self.attributes("-fullscreen", True)
 
     def up_callback(self, displacement, delayz):
         if self.thread_motor is None or not self.thread_motor.is_alive():
